7c8e9dcacba2665c95e50ad7ee099b009e3d9d28
muddassir297 <muddassirhussain297@hotmail.com>
2020-09-30 19:51:51 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerListComponent,
+    CustomerDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
